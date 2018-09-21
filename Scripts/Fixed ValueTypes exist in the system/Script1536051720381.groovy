af33b74b6cee2260ab24e4fbc0e0a7bc7f5d83a8
rkulkarn <rkulkarn@41XHMV1-E6330.callidussoftware.com>
2018-09-21 15:49:14 +0530

    Updated Fixed Value Type, Fixed Value, Fixed Value Variable with No Record Found Validation

--- Scripts/Fixed ValueTypes exist in the system/Script1536051720381.groovy
+++ Scripts/Fixed ValueTypes exist in the system/Script1536051720381.groovy
@@ -19,6 +19,7 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 
 WebUI.callTestCase(findTestCase('Commission Login'), [:], FailureHandling.STOP_ON_FAILURE)
 
@@ -28,61 +29,47 @@ WebUI.mouseOver(findTestObject('Page_Callidus Cloud Commissions Man/div_ADMINIST
 
 WebUI.delay(3)
 
-if (true) {
-    println('User is able to naviagte to Administration tab')
+println('User is able to naviagte to Administration tab')
 
-    WebUI.scrollToElement(findTestObject('Page_Callidus Cloud Commissions Man/a_Fixed Value Types'), 6)
+WebUI.scrollToElement(findTestObject('Page_Callidus Cloud Commissions Man/a_Fixed Value Types'), 6)
 
-    WebUI.delay(4)
+WebUI.delay(4)
 
-    WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/a_Fixed Value Types'))
+WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/a_Fixed Value Types'))
 
-    if (true) {
-        println('User Naviagted to Fixed Value types screen')
+println('User Naviagted to Fixed Value types screen')
 
-        WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/a_Advanced Search'))
+WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/a_Advanced Search'))
 
-        WebUI.delay(3)
+WebUI.delay(3)
 
-        WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/td_Field Name'))
+WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/td_Field Name'))
 
-        WebUI.delay(2)
+WebUI.delay(2)
 
-        WebUI.scrollToElement(findTestObject('Page_Callidus Cloud Commissions Man/option_Fixed Value Type id'), 3)
+WebUI.scrollToElement(findTestObject('Page_Callidus Cloud Commissions Man/option_Fixed Value Type id'), 3)
 
-        WebUI.delay(2)
+WebUI.delay(2)
 
-        WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/option_Fixed Value Type id'))
+WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/option_Fixed Value Type id'))
 
-        WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/td_Comparision'))
+WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/td_Comparision'))
 
-        WebUI.delay(2)
+WebUI.delay(2)
 
-        WebUI.scrollToElement(findTestObject('Page_Callidus Cloud Commissions Man/option_Equals'), 2)
+WebUI.scrollToElement(findTestObject('Page_Callidus Cloud Commissions Man/option_Equals'), 2)
 
-        WebUI.delay(2)
+WebUI.delay(2)
 
-        WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/option_Equals'))
+WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/option_Equals'))
 
-        WebUI.delay(2)
+WebUI.delay(2)
 
-        WebUI.setText(findTestObject('Page_Callidus Cloud Commissions Man/td_Value'), 'Fix1')
+WebUI.setText(findTestObject('Page_Callidus Cloud Commissions Man/td_Value'), FV_Type)
 
-        WebUI.delay(2)
+WebUI.delay(2)
 
-        WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/input_Apply Search'))
+WebUI.click(findTestObject('Page_Callidus Cloud Commissions Man/input_Apply Search'))
 
-        println('User search for Fixed Value Types successfull, Testcase , Pass')
-    } else {
-        false
-    }
-    
-    println('Fixed Value Types screen is unavailable Test case FAIL')
-} else {
-    false.call({ 
-            println('Administration tab is unavailable, Test case Fail')
-        })
-}
-
-WebUI.callTestCase(findTestCase('Commission Logout'), [:], FailureHandling.STOP_ON_FAILURE)
+CustomKeywords.'globalkeywords.record.RecordNotFound'()
 
